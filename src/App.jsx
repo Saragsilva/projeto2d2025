01efe94aca259c0971268558c90872f364beb912
Saragsilva <rm96737@estudante.fieb.edu.br>
2025-03-12 08:25:10 -0300

    ok

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Component} from "react";
+
+class App extends Component{
+
+   render(){
+
+    return(
+
+      <>
+        <div>
+           <h3>aula retomada react vite </h3>
+           <p>Retomanda de criação de projeto</p>
+        </div>
+      </>
+    )
+   }
+}
+ export default App
+ 
+ 
